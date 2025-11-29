64e2f3ca7bc558f891a837570756383c5590d172
Tiago Augusto Welsing <tiagowelsing@gmail.com>
2025-11-29 13:42:28 -0300

    Adicionar arquivos .py e remover .pyc

---
+++ Documents/Codigo/projeto_vital/main.py
@@ -0,0 +1,27 @@
+from cardapio import Cardapio   
+
+
+def main():
+    sistema = Cardapio()       
+
+    while True:
+        sistema.mostrar_cardapio()
+        
+        opcao = input("\nEscolha uma opção: ")
+
+        if opcao == "1":
+            sistema.entradas()
+        elif opcao == "2":
+            sistema.massas()
+        elif opcao == "3":
+            sistema.bebidas()
+        elif opcao == "4":
+            sistema.pedido.mostrar_pedido()
+        elif opcao == "5":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")    
+
+if __name__ == "__main__":
+    main()
